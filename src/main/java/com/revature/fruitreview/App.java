6f3a945e523e71c4f6ab05132dc3f3f34b5c9d95
Granger Carty <grangercarty@gmail.com>
2022-03-08 09:39:00 -0600

    Added tomcat

--- src/main/java/com/revature/fruitreview/App.java
+++ src/main/java/com/revature/fruitreview/App.java
@@ -1,4 +1,29 @@
 package com.revature.fruitreview;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.IOException;
+
 public class App {
+    public static void main(String[] args) {
+        Tomcat server = new Tomcat();
+        server.getConnector();
+        server.addContext("",null);
+        server.addServlet("", "defaultServlet", new HttpServlet() {
+            @Override
+            protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+                resp.getWriter().println("Hello");
+            }
+        }).addMapping("/*");
+        try {
+            server.start();
+        } catch (LifecycleException e) {
+            System.err.println("Server did not start");
+        }
+    }
 }
