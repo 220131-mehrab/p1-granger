9d79966a5520c581f377fa6f906f394dbbbb414f
Granger Carty <grangercarty@gmail.com>
2022-03-11 16:45:05 -0600

    Began adding SQL backend

---
+++ bin/main/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "fruitreviews" (
+    "ReviewId" INT PRIMARY KEY NOT NULL,
+    "FruitName" VARCHAR,
+    "Rating" INT,
+    "Review" VARCHAR,
+);
+
+INSERT INTO "fruitreviews" VALUES (1, 'Apple', 5, 'Much tastier than a PC');
--- src/main/java/com/revature/fruitreview/App.java
+++ src/main/java/com/revature/fruitreview/App.java
@@ -1,16 +1,30 @@
 package com.revature.fruitreview;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
 public class App {
     public static void main(String[] args) {
+        //Connecting to H2 database
+        String url = "jdbc:h2:mem:test";
+        String username = "sa";
+        String password = "";
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        } catch (SQLException eSQL) {
+            System.err.println("Could not find SQL database\n"+eSQL.getMessage());
+        }
+
+        //Starting Tomcat Server
         FruitServer fruitServer = new FruitServer();
         Tomcat server = fruitServer.getTomcat();
         try {
             server.start();
-        } catch (LifecycleException e) {
-            System.err.println("Server did not start");
+        } catch (LifecycleException eLife) {
+            System.err.println("Server did not start\n"+eLife.getMessage());
         }
     }
 }
